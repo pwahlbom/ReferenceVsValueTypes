df7a35b8687160e6d57e7cb947c7636681c6e4b6
Philip Wahlbom <philip@wahlbom.net>
2022-02-06 16:09:11 -0500

    Update Program.cs

--- ReferenceVsValueTypes/Program.cs
+++ ReferenceVsValueTypes/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ReferenceVsValueTypes
 {
+    class MyClass
+    {
+        public int MyProperty { get; set; }
+
+
+        public int MyMethod(int1 intParameter)
+        {
+            return varReturn;
+        }
+    }
+
+    class Prodcut
+    {
+
+        public static void Increment(ref int intParameter)
+        {
+            intParameter = intParameter + 1;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            var intCounter = 0;
+            Prodcut.Increment(ref intCounter);
+            Debug.Assert(intCounter == 1);
+
+
         }
     }
 }
